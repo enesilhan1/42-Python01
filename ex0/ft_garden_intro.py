1403e7873d2a00556aee661cbd9639beac466a99
Enes Ilhan <enilhan@k1m04s06.42istanbul.com.tr>
2026-05-19 13:36:08 +0300

    build: ft_garden_intro has been created and its done but ı need check for flake8 mistakes

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    name = "Rose"
+    height = 15
+    age = 25
+    print("=== Welcome to My Garden ===")
+    print("Plant:", name)
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print()
+    print("=== End of Program ===")
